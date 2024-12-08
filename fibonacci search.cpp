718cc08a338f4e4099e741fe8c8deeb0304f4e7e
mbyar <155609404+mbyar@users.noreply.github.com>
2024-12-08 21:21:50 +0800

    斐波那契查找
    适合于那些要查找的记录在右侧的
    时间复杂度为O(logn)

---
+++ fibonacci search.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int F[100];
+int Fibonacci_Search(int* a, int n, int key)
+{
+	int low, high, mid, i, k;
+	low = 1;
+	high = n;
+	k = 0;
+	while (n > F[k] - 1)
+		k++;
+	for (i = n; i <= F[k] - 1; i++)
+		a[i] = a[n];
+	while (low <= high)
+	{
+		mid = low + F[k - 1] - 1;
+		if (key < a[mid])
+		{
+			high = mid - 1;
+			k = k - 1;
+		}
+		else if (key > a[mid])
+		{
+			low = mid + 1;
+			k = k - 2;
+		}
+		else
+		{
+			if (mid <= n)
+				return mid;
+			else
+				return n;
+		}
+	}
+	return 0;
+}
+int main()
+{
+
+	return 0;
+}
